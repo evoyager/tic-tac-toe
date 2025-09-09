7eb0a9f1317fe48398069d5c65bd2d5de43c4d70
Ievgen Gusar <evgeniygusar@gmail.com>
2025-09-10 00:53:04 +0300

    Configure databaseURL

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,11 +4,12 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  "apiKey": "AIzaSyBnGSGhKWl1zwb9jHa04YrnYNHl1aytKEM",
+  "authDomain": "studio-1326460565-b6c4b.firebaseapp.com",
+  "databaseURL": "https://studio-1326460565-b6c4b-default-rtdb.firebaseio.com",
   "projectId": "studio-1326460565-b6c4b",
   "appId": "1:610707782019:web:bb8d815ae4e620e1c8deb6",
   "storageBucket": "studio-1326460565-b6c4b.firebasestorage.app",
-  "apiKey": "AIzaSyBnGSGhKWl1zwb9jHa04YrnYNHl1aytKEM",
-  "authDomain": "studio-1326460565-b6c4b.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "610707782019"
 };
